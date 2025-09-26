316478d3a5f636fdafe83c092d1cc2613caf2393
Bianca <bpas@cin.ufpe.br>
2025-09-25 21:22:44 -0300

    implementação do exercicio de biblioteca

--- Listas/biblioteca.hs
+++ Listas/biblioteca.hs
@@ -11,16 +11,35 @@ baseExemplo = [("Sergio","O Senhor dos Aneis"),
 -- livros emprestados
 
 livros :: BancoDados -> Pessoa -> [Livro]
-livros ((p, l):b) pessoa =
-    livros ("", []:[]) "" = []
-    livros p == pessoa = l : livros b pessoa
+livros [] _ = []
+livros ((p, l):xs) pessoa
+    | p == pessoa = l : livros xs pessoa
+    | otherwise = livros xs pessoa
 
--- emprestimos :: BancoDados -> Livro ->[Pessoa]
+emprestimos :: BancoDados -> Livro ->[Pessoa]
+emprestimos [] _ = []
+emprestimos ((p, l):xs) livro
+    | l == livro = p : emprestimos xs livro
+    | otherwise = emprestimos xs livro
 
--- emprestado :: BancoDados -> Livro -> Bool
+emprestado :: BancoDados -> Livro -> Bool
+emprestado [] _ = False
+emprestado ((p, l):xs) livro
+    | l == livro = True
+    | otherwise = emprestado xs livro
 
--- qtdEmprestimos :: BancoDados -> Pessoa -> Int
+qtdEmprestimos :: BancoDados -> Pessoa -> Int
+qtdEmprestimos [] _ = 0
+qtdEmprestimos ((p, l):xs) pessoa
+    | p == pessoa = 1 + qtdEmprestimos xs pessoa
+    | otherwise = qtdEmprestimos xs pessoa
 
--- emprestar :: BancoDados -> Pessoa -> Livro -> BancoDados
+emprestar :: BancoDados -> Pessoa -> Livro -> BancoDados
+emprestar (a:xs) pessoa livro = a : emprestar xs pessoa livro
+emprestar [] pessoa livro = [(pessoa, livro)]
 
--- devolver :: BancoDados -> Pessoa -> Livro -> BancoDados
+devolver :: BancoDados -> Pessoa -> Livro -> BancoDados
+devolver [] _ _ = []
+devolver ((p, l):xs) pessoa livro
+    | p == pessoa && l == livro = xs
+    | otherwise = (p, l) : devolver xs pessoa livro
